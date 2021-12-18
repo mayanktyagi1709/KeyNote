ba0374aaa5818b962e4f0232d8eea61d5934b399
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2021-12-18 19:52:13 +0530

    Setup Backend folder

---
+++ Backend/db.js
@@ -0,0 +1,13 @@
+// connecting mongoDB to our entry point (or to our project)
+
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+
+// function to connect to mongoDB
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {
+        console.log("Mongoose connected succesfully!");
+    })
+}
+
+module.exports = connectToMongo;
