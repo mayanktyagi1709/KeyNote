fec97841daeca552ec6b20a03710bb739e892e04
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2021-12-19 13:55:16 +0530

    Created routes and schema

---
+++ Backend/models/Notes.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const { stringify } = require('nodemon/lib/utils');
+
+const NotesSchema = new Schema({
+    title: {
+        type: string,
+        required: true
+    },
+    description: {
+        type: string,
+        required: true,
+    },
+    tag: {
+        type: string,
+        default: 'General'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.model('notes', NotesSchema);
