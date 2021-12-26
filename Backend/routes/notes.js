23f8898b48ec56dd8c013c63539b732d57783129
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2021-12-26 12:02:23 +0530

    Added updatenote end point

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -48,4 +48,38 @@ router.post('/addnote', fetchuser, [
     
 });
 
+// ROUTE-3 
+// Update an existing note using PUT: "/api/notes/updatenote"- Login required.
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+
+  try {
+    const {title, description, tag} = req.body;
+
+    // creating an empty object
+    const newNote = {};
+    // if user is sending an updated title or desc or tag, then add it to this empty object
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+
+    // find the id of the note to be updated
+    let note = await Note.findById(req.params.id);
+    // if note does not exist
+    if(!note){return res.status(404).send("Not found")};
+    // if the id of the note does not match the id of user requesting the update
+    if(note.user.toString() !== req.user.id)
+    {
+      return res.status(401).send("Not allowed");
+    }
+    // if everything is fine, update the note 
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
+    res.json({note});
+  } 
+  catch (error) {
+    console.error(error.message);
+    res.status(500).send('Internal Error Ocuured');
+  }
+  
+});
+
 module.exports = router;
