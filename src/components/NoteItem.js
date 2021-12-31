98ea53ab5209861a9476ebe0ea738b0615802057
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2021-12-31 19:41:30 +0530

    Added update modal

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -5,10 +5,10 @@ const NoteItem = (props) => {
     const context = useContext(noteContext)
     const {deleteNote} = context;
 
-    // notes is passed from Notes.js and it contains the information 
+    // note is passed from Notes.js and it contains the information 
     // like title, description, and tag of a note.
     // We are iterating over each note in Notes.js and passing note as a prop
-    const {note} = props
+    const {note, updateNote} = props
     return (
         <div className="col-md-3">
             <div className="card">
@@ -21,8 +21,8 @@ const NoteItem = (props) => {
                         {note.tag}
                     </span>
                         <h5 className="card-title">{note.title}</h5>
-                        <i className="fas fa-trash mx-3" onClick = {()=>{deleteNote(note._id)}}></i>
-                        <i className="fas fa-edit mx-3"></i>
+                        <i className="fas fa-trash mx-2" onClick = {()=>{deleteNote(note._id)}}></i>
+                        <i className="fas fa-edit mx-2" onClick={()=>{updateNote(note)}}></i>
                     </div>
                     <p className="card-text">{note.description}</p>
                 </div>
